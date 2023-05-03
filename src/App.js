bc91452901436c937876a8ba71d84711ca53d60d
emilio aguirre <emilio94avv@gmail.com>
2023-05-03 17:34:29 -0300

    agrego navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Profile from "./MyProfile";
+import NavBar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <NavBar />
+      </header>
       <body>
         <Profile></Profile>
         <Home></Home>
